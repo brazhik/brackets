6e021ffc9299ca084197486b604fe7586bd10096
Dmitrii <mail@example.com>
2022-07-17 14:09:37 +0300

    feat: implement check function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
-module.exports = function check(str, bracketsConfig) {
-  // your solution
+const getCloseBracket = (bracket, bracketsConfig) => {
+  for (let item of bracketsConfig) {
+    if (item[0] === bracket) return item[1];
+  }
 }
+
+module.exports = function check(str, bracketsConfig) {
+  let bracketsStack = [];
+
+  for (let i = 0; i < str.length; i++) {
+    let closeBracket = getCloseBracket(str[i], bracketsConfig);
+
+    if ((bracketsStack.length === 0 || bracketsStack.at(-1) !== str[i]) && (closeBracket === undefined)) {
+      return false;
+    }
+
+    if (bracketsStack.at(-1) === str[i]) {
+      bracketsStack.pop();
+    } else if (closeBracket !== undefined) {
+      bracketsStack.push(closeBracket);
+    }
+  }
+
+  return (bracketsStack.length === 0);
+}
+
+// console.log(module.exports('|(|)|', [['(', ')'], ['|', '|']]));
